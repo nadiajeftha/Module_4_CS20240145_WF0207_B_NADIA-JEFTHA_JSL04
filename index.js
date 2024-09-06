683c42337d77592fbc4076cc4477cecfd7f9b149
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-09-06 14:19:09 +0200

    correct console logging

--- index.js
+++ index.js
@@ -1,53 +1,18 @@
-// # [JSL04] Submission: Monster Ternary Operator
+let userPreference = ''; //'Regular', 'Sugar-free', or undefined
+console.log(userPreference === 'Sugar-free' ? 'Sugar-free' : 'Regular'); //if input says 'sugar-free' , prints out 'sugar-free'. if input says nothing or 'regular' , prints out regular.
 
-// #### Challenge 1: Flavor Selection
-// Given a variable that holds a user's preferred type of Monster Energy drink, 
-// print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
-
-let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
-
-// #### Challenge 2: Stock Check
-// You have a variable that tracks the number of Monster Energy cans left in the fridge. 
-// Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
-
-let cansLeft = 3; // Any number of cans
-console.log(``);
+let cansLeft = 5; // Any number of cans
+console.log(cansLeft < 5 ? "TIME TO RESTOCK!" : "WE'RE STOCKED!"); //if there are less than 5 cans left,restock. if 5 and more left,stocked
 
 
-// #### Challenge 3: Workout Intensity
-// Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
-// If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
-
-let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+let heartRate = 100; // Current heart rate in bpm
+console.log(heartRate < 100 ? "BOOST NEEDED!" : "ENERGY LEVELS ARE HIGH!"); // if heartrate is under 100,boost needed. if heartrate 100 and + , energy levels too high
 
 
-// #### Challenge 4: Temperature Suitability
-// Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
-// if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
-
-let currentTemp = 4; // Current temperature in °C
-console.log(``);
+let currentTemp = 6; // Current temperature in °C
+console.log(currentTemp<= 5 ? "CHILLED TO PERFECTION!" : "NEEDS A COOLER!"); //if temp higher than 5,need to cool. if 5 and lower,no need to cool.
 
 
-// #### Challenge 5: Late Night Coding Session
-// Determine if it's a good idea to have a Monster Energy drink based on the current hour.
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!",
-// otherwise, print "Better stick to water."
+let currentHour = 1; //current time in 24-hr format
+console.log(currentHour >=7 && currentHour <24 ? "UNLEASH THE BEAST!" : "BETTER STICK TO WATER.") //if time is between 7am - 12am, drink energy drink. otherwise,drink water.
 
-// Define the current hour in 24-hour format
-let currentHour = 22; 
-
-// Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
-// Otherwise, print "Better stick to water."
-
-
-
-
-
-
-// In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
-// demonstrating its utility for concise conditional logic. Remember that while ternaries can make the code more succinct, 
-// they should be used judiciously to maintain readability, especially for more complex conditions.
